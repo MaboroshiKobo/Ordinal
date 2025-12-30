df5e435a8e101c9953226933847d51eb49e8b3c0
Muhammad Fardeen <m.fdeen02@gmail.com>
2025-12-30 17:53:23 -0600

    feat: create internal PAPI expansion and add missing config

--- src/main/java/org/maboroshi/ordinal/Ordinal.java
+++ src/main/java/org/maboroshi/ordinal/Ordinal.java
@@ -2,22 +2,25 @@ package org.maboroshi.ordinal;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.maboroshi.ordinal.config.ConfigManager;
+import org.maboroshi.ordinal.hook.OrdinalExpansion;
 import org.maboroshi.ordinal.listener.JoinListener;
 import org.maboroshi.ordinal.manager.OrdinalManager;
 import org.bstats.bukkit.Metrics;
 
 public final class Ordinal extends JavaPlugin {
-    private static Ordinal plugin;
     private ConfigManager configManager;
     private OrdinalManager ordinalManager;
 
     @Override
     public void onEnable() {
-        plugin = this;
         this.configManager = new ConfigManager(this);
         this.ordinalManager = new OrdinalManager(this);
 
         getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new OrdinalExpansion(this).register();
+            getLogger().info("PlaceholderAPI hook enabled.");
+        }
 
         @SuppressWarnings("unused")
         Metrics metrics = new Metrics(this, 28615);
--- src/main/java/org/maboroshi/ordinal/hook/OrdinalExpansion.java
+++ src/main/java/org/maboroshi/ordinal/hook/OrdinalExpansion.java
@@ -1,5 +1,58 @@
 package org.maboroshi.ordinal.hook;
 
-public class OrdinalExpansion {
-    
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.maboroshi.ordinal.Ordinal;
+import org.maboroshi.ordinal.manager.OrdinalManager;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+public class OrdinalExpansion extends PlaceholderExpansion {
+    private final Ordinal plugin;
+    private final OrdinalManager ordinalManager;
+
+    public OrdinalExpansion(Ordinal plugin) {
+        this.plugin = plugin;
+        this.ordinalManager = plugin.getOrdinalManager();
+    }
+
+    @Override
+    @NotNull
+    public String getAuthor() {
+        return String.join(", ", plugin.getPluginMeta().getAuthors());
+    }
+
+    @Override
+    @NotNull
+    public String getIdentifier() {
+        return "ordinal";
+    }
+
+    @Override
+    @NotNull
+    public String getVersion() {
+        return plugin.getPluginMeta().getVersion();
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, @NotNull String params) {
+        if (player == null) {
+            return "";
+        }
+
+        if (params.equalsIgnoreCase("id")) {
+            int rank = ordinalManager.getOrdinal(player);
+
+            if (rank == -1) {
+                return "";
+            }
+            return String.valueOf(rank);
+        }
+        return null;
+    }
 }
